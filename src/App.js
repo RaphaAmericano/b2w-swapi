47d8a5d29bb4b96a9b31c6665154a1f673caa028
RaphaAmericano <raphael@raphaelamericano.com.br>
2018-09-15 00:54:39 -0300

    Carregando API e botao next funcionando

--- src/App.js
+++ src/App.js
@@ -1,18 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import Filmes from './Filmes';
 
 class App extends Component {
+
+  
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dados: [],
+      filmes: []
+    }
+    // this.loadPlanet();
+  }
+
+  loadPlanet = () => {
+    let numberPlanet = Math.floor(Math.random() * 61) + 1;
+    axios.get('https://swapi.co/api/planets/'+numberPlanet)
+      .then(res => {
+        const planeta = res.data;
+        const filmes = this.loadFilms(res.data.films);
+        this.loadFilms(res.data.films);
+        this.setState( { dados: planeta } );
+        this.setState( { filmes: filmes } );
+      });
+  }
+
+  loadFilms(arr){
+    let retorno = [];
+    for(let i = 0; i < arr.length; i++){
+      axios.get(arr[i])
+        .then(res => {
+          retorno.push(res.data);
+        })
+    }
+    console.log(retorno);
+    return retorno;
+  }
+
+  onChange = (event) => {
+    this.setState({teste: event.target.value})
+  }
+
   render() {
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>{this.state.dados.name}</h1>
+        <h2>Population: {this.state.dados.population}</h2>
+        <h3>Climate: {this.state.dados.climate}</h3>
+        <h4>Terrain: {this.state.dados.terrain}</h4>
+        <h5>Featured in films</h5>
+        
+        <button onClick={this.loadPlanet}>Next</button>
       </div>
     );
   }
