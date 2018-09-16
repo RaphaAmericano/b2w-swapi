f98ae79d1ceceb291d748d6cec3bcf3fac9b0b82
RaphaAmericano <raphael@raphaelamericano.com.br>
2018-09-15 23:53:59 -0300

    Funcionando

--- src/App.js
+++ src/App.js
@@ -1,59 +1,56 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
-import Filmes from './Filmes';
 
 class App extends Component {
-
   
-
   constructor(props) {
     super(props);
     this.state = {
-      dados: [],
+      planeta: [],
       filmes: []
     }
-    // this.loadPlanet();
+    
   }
 
   loadPlanet = () => {
     let numberPlanet = Math.floor(Math.random() * 61) + 1;
-    axios.get('https://swapi.co/api/planets/'+numberPlanet)
+   
+    let retorno = [];
+    axios.get('https://swapi.co/api/planets/'+1)
       .then(res => {
-        const planeta = res.data;
-        const filmes = this.loadFilms(res.data.films);
-        this.loadFilms(res.data.films);
-        this.setState( { dados: planeta } );
-        this.setState( { filmes: filmes } );
+        let filmes = res.data.films;
+        this.setState( { planeta: res.data } );
+        for(let i = 0; i < filmes.length; i++){
+          axios.get(filmes[i])
+            .then(res => {
+              let filme = res.data.title;  
+              this.setState(prev => ({ filmes: [...prev.filmes, filme] }) );
+          }).catch(err => console.log(err));
+        } 
       });
   }
 
-  loadFilms(arr){
-    let retorno = [];
-    for(let i = 0; i < arr.length; i++){
-      axios.get(arr[i])
-        .then(res => {
-          retorno.push(res.data);
-        })
-    }
-    console.log(retorno);
-    return retorno;
-  }
-
-  onChange = (event) => {
-    this.setState({teste: event.target.value})
+  componentDidMount(){
+    this.loadPlanet(); 
   }
 
   render() {
-
+    //let teste = [ 'joao', 'jose', 'maria', 'marcos'];
+    // const testeLista = teste.map(pessoa => <li>{pessoa}</li>);
+    //let info = this.state.filmes;
+    const lista = this.state.filmes.map( filme => <li>{filme}</li>);
+    //console.log(teste);
+    //console.log(info);
+    
     return (
       <div className="App">
-        <h1>{this.state.dados.name}</h1>
-        <h2>Population: {this.state.dados.population}</h2>
-        <h3>Climate: {this.state.dados.climate}</h3>
-        <h4>Terrain: {this.state.dados.terrain}</h4>
+        <h1>{this.state.planeta.name}</h1>
+        <h2>Population: {this.state.planeta.population}</h2>
+        <h3>Climate: {this.state.planeta.climate}</h3>
+        <h4>Terrain: {this.state.planeta.terrain}</h4>
         <h5>Featured in films</h5>
-        
+        <ul>{lista}</ul>
         <button onClick={this.loadPlanet}>Next</button>
       </div>
     );
