df1302f7b489dbeda551c99683007f5b3edb3efd
RaphaAmericano <raphael@raphaelamericano.com.br>
2018-09-16 15:58:45 -0300

    Adiconando CSS

--- src/App.js
+++ src/App.js
@@ -8,26 +8,33 @@ class App extends Component {
     super(props);
     this.state = {
       planeta: [],
-      filmes: []
+      filmes: [],
+      possuiFilmes: false
     }
-    
   }
 
   loadPlanet = () => {
     let numberPlanet = Math.floor(Math.random() * 61) + 1;
-   
-    let retorno = [];
-    axios.get('https://swapi.co/api/planets/'+1)
+    this.setState({ filmes: [] } );
+    axios.get('https://swapi.co/api/planets/'+numberPlanet)
       .then(res => {
         let filmes = res.data.films;
+        console.log(filmes);
         this.setState( { planeta: res.data } );
+        
+        if(filmes.length > 0 ){
+          this.setState( { possuiFilmes: true } );
+        } else {
+          this.setState( { possuiFilmes: false } );
+        }
+
         for(let i = 0; i < filmes.length; i++){
           axios.get(filmes[i])
             .then(res => {
               let filme = res.data.title;  
               this.setState(prev => ({ filmes: [...prev.filmes, filme] }) );
           }).catch(err => console.log(err));
-        } 
+        }
       });
   }
 
@@ -36,22 +43,44 @@ class App extends Component {
   }
 
   render() {
-    //let teste = [ 'joao', 'jose', 'maria', 'marcos'];
-    // const testeLista = teste.map(pessoa => <li>{pessoa}</li>);
-    //let info = this.state.filmes;
-    const lista = this.state.filmes.map( filme => <li>{filme}</li>);
-    //console.log(teste);
-    //console.log(info);
-    
+    const itens = this.state.filmes.map( (filme, i) => 
+        <li className="collection-item" key={i}>{filme}</li>    
+    );
+    let lista;
+    if(this.state.possuiFilmes){
+    lista = (
+      <ul className="collection with-header">
+        <li className="collection-header"><h5>Featured in films</h5></li>
+        {itens}
+      </ul>
+    )
+    }
     return (
-      <div className="App">
-        <h1>{this.state.planeta.name}</h1>
-        <h2>Population: {this.state.planeta.population}</h2>
-        <h3>Climate: {this.state.planeta.climate}</h3>
-        <h4>Terrain: {this.state.planeta.terrain}</h4>
-        <h5>Featured in films</h5>
-        <ul>{lista}</ul>
-        <button onClick={this.loadPlanet}>Next</button>
+      <div className="container">
+        <div className="row">
+          <div className="col s10">
+            <div className="card">
+              <div className="card-content">
+                <h1>{this.state.planeta.name}</h1>
+                <div className="divider"></div>
+                <h3>Population: {this.state.planeta.population}</h3>
+                <h3>Climate: {this.state.planeta.climate}</h3>
+                <h4>Terrain: {this.state.planeta.terrain}</h4>
+          
+                  
+                    {lista}
+                  
+          
+                
+                <div className="card-action">
+                  <button className="waves-light btn center-align" onClick={this.loadPlanet}>Next</button>
+                </div>
+              </div>
+            </div>
+            
+            
+          </div>
+        </div>
       </div>
     );
   }
